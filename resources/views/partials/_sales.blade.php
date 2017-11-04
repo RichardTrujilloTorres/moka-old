4a013b06735aeac9098e63023ddbec36647b64b9
Richard Trujillo <richard@localhost.localdomain>
2017-11-04 20:34:38 +0100

    Created required dirs.

---
+++ resources/views/partials/_sales.blade.php
@@ -0,0 +1,21 @@
+<!-- sales card -->
+<div class="card ">
+    <div class="header">
+        <h4 class="title">2015 Sales</h4>
+        <p class="category">All products including Taxes</p>
+    </div>
+    <div class="content">
+        <div id="chartActivity" class="ct-chart"></div>
+
+        <div class="footer">
+            <div class="chart-legend">
+                <i class="fa fa-circle text-info"></i> Tesla Model S
+                <i class="fa fa-circle text-warning"></i> BMW 5 Series
+            </div>
+            <hr>
+            <div class="stats">
+                <i class="ti-check"></i> Data information certified
+            </div>
+        </div>
+    </div>
+</div>
