89f8a20c7b7a1507a3c255ee6ab4ad7c8cc417b2
Richard Trujillo <richard.trujillo.torres@gmail.com>
2017-12-17 19:24:15 +0100

    Updated welcome page links.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        {{-- <a href="{{ route('admin.dashboard') }}">Dashboard</a> --}}
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
@@ -85,7 +85,7 @@
                 <div class="links">
                     <a href="{{ route('admin.dashboard') }}">Go to the Panel</a>
                     <a href="">Documentation</a>
-                    <a href="{{ env('APP_GITHUB_URL') }}">GitHub</a>
+                    <a href="{{ env('APP_GITHUB_URL') }}" target="_blank">GitHub</a>
                 </div>
             </div>
         </div>
