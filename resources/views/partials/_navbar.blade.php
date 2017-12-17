d5badf36a65e1a0845acb0d1fc2daf19931aa2a8
Richard Trujillo <richard.trujillo.torres@gmail.com>
2017-12-17 19:26:29 +0100

    Added navbar partial.

---
+++ resources/views/partials/_navbar.blade.php
@@ -0,0 +1,79 @@
+<!-- Main navbar -->
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar bar1"></span>
+                <span class="icon-bar bar2"></span>
+                <span class="icon-bar bar3"></span>
+            </button>
+            <a class="navbar-brand" href="#">@yield('title', '')</a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <!-- search -->
+                <li>
+                    <input class="form-control" type="search" id="search" name="search">
+                </li>
+                <li>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="ti-panel"></i>
+                        <p>Stats</p>
+                    </a>
+                </li>
+                <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="ti-bell"></i>
+                            <p class="notification">5</p>
+                            <p>Notifications</p>
+                            <b class="caret"></b>
+                      </a>
+                      <ul class="dropdown-menu">
+                        @if (empty(@$notifications))
+                        <li><a href="">You have no notifications.</a></li>
+                        @else
+                        <li><a href="{{ $notification->url }}">{{ $notification->description }}</a></li>
+                        @endif
+                      </ul>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="ti-settings"></i>
+                        <p>Settings</p>
+                    </a>
+                </li>
+
+
+
+                <!-- Authentication Links -->
+                @guest
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
+
+            </ul>
+
+        </div>
+    </div>
+</nav>
