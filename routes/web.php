40eefc0fba7409d1f4ff312fa34607f3c65f6c43
Richard Trujillo <richard.trujillo.torres@gmail.com>
2018-02-25 20:35:49 +0100

    Clean file up

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,23 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Pages
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+
+
+/*
+|--------------------------------------------------------------------------
+| Admin
+|--------------------------------------------------------------------------
+*/
 Route::namespace('Admin')->group(function () {
     Route::middleware(['web', 'auth'])->group(function() {
         Route::prefix('/admin')->group(function () {
@@ -30,3 +30,5 @@ Route::namespace('Admin')->group(function () {
     });
 });
 
+
+Auth::routes();
