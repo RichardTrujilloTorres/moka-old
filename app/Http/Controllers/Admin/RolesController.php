5102142654ac3e12451dbdf2cfbd894489b3fad8
Richard Trujillo <richard.trujillo.torres@gmail.com>
2018-03-10 23:05:42 +0100

    Add delete and update procedures

--- app/Http/Controllers/Admin/RolesController.php
+++ app/Http/Controllers/Admin/RolesController.php
@@ -27,5 +27,87 @@ class RolesController extends Controller
         return view('roles.index')->with(compact('roles'));
     }
 
+    /**
+     * Show the form to create a new role.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('roles.create');
+    }
+
+    /**
+     * Create a role.
+     *
+     * @param \App\Http\Requests\StoreRoleRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRoleRequest $request)
+    {
+
+        // @todo allow the possibility to add permission(s) on 
+        // role creation
+
+        $role = Role::create([
+            'name' => $request->name,
+            //
+        ]);
+
+        if (! $role) {
+            return new \Exception("Could not create role.");
+        }
+
+        return redirect()->route('admin.roles')->with([
+            'message' =>  'Role created.',
+            'status' => 'success',
+        ]);
+    }
+
+    /**
+     * Edit a role.
+     *
+     * @param \Spatie\Permission\Models\Role $role
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Role $role)
+    {
+        return view('roles.edit')->with(compact('role'));
+    }
+
+    /**
+     * Update a role.
+     *
+     * @param \Spatie\Permission\Models\Role $role
+     * @param \App\Http\Requests\StoreRoleRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Role $role, StoreRoleRequest $request)
+    {
+        $role->update($request->all());
+
+        return redirect()->route('admin.roles')->with([
+            'message' =>  'Role update.',
+            'status' => 'success',
+        ]);
+    }
+
+
+    /**
+     * Delete a role.
+     *
+     * @param \Spatie\Permission\Models\Role $role
+     * @return \Illuminate\Http\Response
+     */
+    public function delete(Role $role)
+    {
+        $role->delete();
+
+        return redirect()->back()->with([
+            'message' =>  'Role deleted.',
+            'status' => 'success',
+        ]);
+    }
+
     //
 }
