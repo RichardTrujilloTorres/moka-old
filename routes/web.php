d9b652c176e97a1024d39d5478e409b308de7c8f
Richard Trujillo <richard.trujillo.torres@gmail.com>
2018-03-10 23:06:54 +0100

    Add roles routes

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,11 @@ Route::namespace('Admin')->group(function () {
             Route::get('dashboard', 'DashboardController@index')->name('admin.dashboard');
 
             Route::get('/users/roles', 'RolesController@index')->name('admin.roles');
+            Route::post('/users/roles', 'RolesController@store')->name('admin.roles.store');
+            Route::get('/users/roles/{role}/edit', 'RolesController@edit')->name('admin.roles.edit');
+            Route::put('/users/roles/{role}', 'RolesController@update')->name('admin.roles.update');
+            Route::get('/users/roles/create', 'RolesController@create')->name('admin.roles.create');
+            Route::delete('/users/roles/{role}', 'RolesController@delete')->name('admin.roles.delete');
         });
     });
 });
