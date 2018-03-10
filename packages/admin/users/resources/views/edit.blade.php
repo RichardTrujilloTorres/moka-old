3be6c4e7855ab07971a400cb45fbf51847c1e14a
Richard Trujillo <richard.trujillo.torres@gmail.com>
2018-03-10 23:13:35 +0100

    Add Google Autocomplete Address Form
    The whole procedure to add Google Autocomplete Address Form. It uses
    the onView object to contain map the corresponding fields on the view.
    To refactor to be reusable.

--- packages/admin/users/resources/views/edit.blade.php
+++ packages/admin/users/resources/views/edit.blade.php
@@ -365,6 +365,66 @@ User Profile
 
 </div>
 
+<script>
+
+    var placeSearch, autocomplete;
+    var componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        country: 'long_name',
+        postal_code: 'short_name'
+      };
+
+    var onView = {
+        locality: 'city',
+        country: 'country',
+        postal_code: 'zip_code'
+    };
+
+
+    // Component initialization
+    function initAutocomplete() {
+        var input = document.getElementById('address');
+        autocomplete = new google.maps.places.Autocomplete(input, {types: ['geocode']});
+
+        autocomplete.addListener('place_changed', fillInAddress);
+    };
+
+    // Form autocompletion
+    function fillInAddress() {
+
+        // Get the place details from the autocomplete object.
+        var place = autocomplete.getPlace();
+
+        for (var component in componentForm) {
+            if (onView.hasOwnProperty(component)) {
+                  document.getElementById(onView[component]).value = '';
+                  document.getElementById(onView[component]).disabled = false;
+            }
+        }
+
+        // Get each component of the address from the place details
+        // and fill the corresponding field on the form.
+        for (var i = 0; i < place.address_components.length; i++) {
+          var addressType = place.address_components[i].types[0];
+          if (componentForm[addressType]) {
+            var val = place.address_components[i][componentForm[addressType]];
+
+            // document.getElementById(addressType).value = val;
+            if (onView.hasOwnProperty(addressType)) {
+                document.getElementById(onView[addressType]).value = val;
+            }
+          }
+        }
+  }
+
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_PLACES_AUTOCOMPLETE_KEY') }}&libraries=places&callback=initAutocomplete"></script>
+
+
 
 @endsection
 
