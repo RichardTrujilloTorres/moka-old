2650c1fcab1469faadb2d5d75c9aee86c46ac5f6
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-07 10:45:13 +0200

    Set sidebar as a partial

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -33,58 +33,8 @@
 <body>
 
 <div class="wrapper">
-	<div class="sidebar" data-background-color="black" data-active-color="success">
 
-    <!--
-		Tip 1: you can change the color of the sidebar's background using: data-background-color="white | black"
-		Tip 2: you can change the color of the active button using the data-active-color="primary | info | success | warning | danger"
-	-->
-
-    	<div class="sidebar-wrapper">
-            <div class="logo">
-                <a href="{{ url('/') }}" class="simple-text">
-                    {{ config('app.name') }}
-                </a>
-            </div>
-
-            <ul class="nav">
-
-                @if (Route::is('admin.dashboard'))
-                <li class="active">
-                @else
-                <li>
-                @endif
-                    <a href="{{ route('admin.dashboard') }}">
-                        <i class="ti-pie-chart"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-
-                @if (Route::is('admin.users.*') || Route::is('admin.users'))
-                <li class="active">
-                @else 
-                <li>
-                @endif
-                    <a href="{{ route('admin.users') }}">
-                        <i class="ti-user"></i>
-                        <p>Users</p>
-                    </a>
-                </li>
-
-                @if (Route::is('admin.roles.*') || Route::is('admin.roles'))
-                <li class="active">
-                @else 
-                <li>
-                @endif
-                    <a href="{{ route('admin.roles') }}">
-                        <i class="ti-lock"></i>
-                        <p>Roles</p>
-                    </a>
-                </li>
-
-            </ul>
-    	</div>
-    </div>
+    @include('partials._sidebar')
 
     <div class="main-panel">
 
