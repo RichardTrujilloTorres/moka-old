5d82d9ae5788d4100446768e5dce1c0b12b8f725
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-07 11:03:52 +0200

    Rename admin users (listing) route

--- packages/admin/users/routes.php
+++ packages/admin/users/routes.php
@@ -4,7 +4,9 @@ Route::prefix(config('users.routes.prefix'))->group(function () {
     Route::middleware(config('users.routes.middleware', 'web'))->group(function () {
         // Route::middleware(['web', 'auth'])->group(function () {
         Route::namespace(config('users.routes.namespace'))->group(function () {
-            Route::get('/', 'UsersController@index')->name('admin.users');
+
+            // TODO rebuild as a resource
+            Route::get('/', 'UsersController@index')->name('admin.users.index');
             Route::get('/{user}/edit', 'UsersController@edit')->name('admin.users.edit');
             Route::get('/{user}', 'UsersController@show')->name('admin.users.show');
             Route::put('/{user}', 'UsersController@update')->name('admin.users.update');
