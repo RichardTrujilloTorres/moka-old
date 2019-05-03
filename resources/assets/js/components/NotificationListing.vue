0d3e06e6fb18e3dec050c4315cc3a9a2b2c9c2d5
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-05-03 18:50:39 +0200

    Add notification listing component

---
+++ resources/assets/js/components/NotificationListing.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul class="dropdown-menu">
+        <li v-if="!notifications.length"><a href="">You have no notifications.</a></li>
+
+        <li v-if="notifications.length" v-for="notification in notifications">
+            <a href="">{{ notification.data.text }}</a>
+        </li>
+    </ul>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data: () => ({
+            notifications: []
+        }),
+
+        created() {
+            // TODO resource building
+            axios.get('/api/notifications')
+                .then(res => this.notifications = res.data.data.notifications)
+                .catch(res => console.log(res))
+        },
+    }
+</script>
