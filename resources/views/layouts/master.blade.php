54b80dfe0c1e9cf0f1f182e6bc1668efdcebf33c
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-05-03 18:51:26 +0200

    Add csrf token to main layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,9 @@
 	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
 	<title>{{ config('app.name') }}</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
