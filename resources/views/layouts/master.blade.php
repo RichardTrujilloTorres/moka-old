51b5569bc5caba7f8d7a52887c1fc721fac6eea4
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-05-16 23:23:50 +0200

    Add app CSS file to master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,6 +14,8 @@
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Bootstrap core CSS     -->
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
