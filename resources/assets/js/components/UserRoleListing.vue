37f36fcf9fdbd8928d89c1b5ff6a6ff984ed5722
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-05-17 23:36:53 +0200

    Add user-role listing component

---
+++ resources/assets/js/components/UserRoleListing.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="user-role-listing">
+        <div class="row">
+            <div class="col-md-6">
+                <h5>Users</h5>
+                <ul>
+                    <li v-for="user in users">
+                        {{ user.name }} ({{ user.email }})
+                        <span class="custom__remove" @click="removeUser(user)">❌</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <h5>Roles</h5>
+                <ul>
+                    <li v-for="role in roles">
+                        {{ role.name }}
+                        <span class="custom__remove" @click="removeRole(role)">❌</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            users: {
+                type: Array,
+                required: false,
+                default: []
+            },
+            roles: {
+                type: Array,
+                required: false,
+                default: []
+            }
+        },
+        data: () => ({
+        }),
+
+        computed: {
+        },
+
+        methods: {
+            removeUser(user) {
+                this.$emit('remove-user', user)
+            },
+            removeRole(role) {
+                this.$emit('remove-role', role)
+            }
+        },
+
+        created() {
+        },
+
+    }
+</script>
