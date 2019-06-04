a275cac1d95e1cbf8acf32633554c7fdf8a29388
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-06-04 07:46:25 +0200

    Add users-roles association view

---
+++ resources/views/users-roles/associate.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('title')
+    User(s)-Role(s) Association
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <user-role-association></user-role-association>
+    </div>
+</div>
+
+@endsection
